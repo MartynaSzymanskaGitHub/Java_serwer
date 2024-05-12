48afb1d0a4548a29f6d9de853c525cefbfcb1136
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-12 20:54:24 +0200

    add more strict Validation to klientHandler

--- klientHandler.java
+++ klientHandler.java
@@ -9,8 +9,27 @@ public class klientHandler extends Thread {
     private PrintWriter out;
     private BufferedReader in;
 
-    public klientHandler(Socket socket) {
+    public klientHandler(Socket socket, serwer Serwer) {
         this.clientSocket = socket;
+    }
+
+    int convertStringToTime(String text){
+        int num = 0;
+        int factor = 1;
+        for (int i = text.length() - 1; i >= 0; i--) {
+            char c = text.charAt(i);
+            if (Character.isDigit(c)) {
+                int digitValue = c - '0';
+                num += factor * digitValue;
+                factor *= 10;
+            }
+        }
+        return num;
+    }
+    String changeString(String inputLine){
+        String divide = ":";
+        int firstIndexOfDivide = inputLine.indexOf(divide);
+        return inputLine.substring(0, firstIndexOfDivide);
     }
 
     public void run() {
@@ -19,24 +38,41 @@ public class klientHandler extends Thread {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                if ("".equals(inputLine)) {
-                    out.println("bye");
-                    break;
-                }
-                String newInput = inputLine;
-                System.out.println("Message from client: "+inputLine);
-                int czas = inputLine.length()-1;
-                ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-                executorService.schedule(() -> {
-                    out.println("Server response after delay: " + newInput);
-                }, czas, TimeUnit.SECONDS);
-                executorService.shutdown(); // Zamknij executor po wykonaniu zadania
+            while (true) { 
+                try {
+                    inputLine = in.readLine();
+                    if(inputLine == null || inputLine.equalsIgnoreCase("exit")){
+                        System.out.println("Connection broken. Disconnect client");
+                        out.close();
+                        in.close();
+                        clientSocket.close();
+                        break;
+                    }
+                    String newInput = changeString(inputLine);
+                    System.out.println("Message from client: " + newInput);
+                    int time = convertStringToTime(inputLine);
+                    ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+                    executorService.schedule(() -> {
+                        out.println("Server response after delay: " + newInput);
+                    }, time, TimeUnit.SECONDS);
+                    executorService.shutdown();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;  
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } 
             }
-             
-            
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (out != null) out.close();
+                if (in != null) in.close();
+                if (clientSocket != null) clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Error closing resources: " + e.getMessage());
+            }
         }
     }
 }
