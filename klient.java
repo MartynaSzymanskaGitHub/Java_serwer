b0008c5b02a22f27a7bcc87f93ba46d112f9cad9
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-12 20:54:51 +0200

    add error to klient.java

--- klient.java
+++ klient.java
@@ -25,6 +25,56 @@ public class klient {
         out.close();
         clientSocket.close();
     }
+
+    static int convertStringToTime(String text){
+        int num = 0;
+        int factor = 1;  
+        int numberlen = 0;
+        boolean isIntiger = false;
+        boolean isSpace = false;
+        
+        if (isNegative(text)) {
+            return -1;
+        }
+        for (int i = text.length() - 1; i >= 0; i--) {
+            char c = text.charAt(i);
+            if (Character.isDigit(c)) {
+                if(isSpace){
+                    return -1;
+                }
+                isIntiger = true;
+                int digitValue = c - '0';
+                num += factor * digitValue;  
+                factor *= 10;  
+                numberlen++;
+            }
+            if((c == ',' || c =='.') && isIntiger == true){
+                return -2;
+            }
+            if(c ==' ' && isIntiger == true ){
+                isSpace = true;
+            }
+        }
+        if (numberlen < 1) {
+            return -1;
+        }
+        return num;
+    }
+
+    static boolean isNegative(String text){
+        char character2 =' '; 
+        for (int i = text.length() - 1; i >= 0; i--) {
+            char character = text.charAt(i);
+            if (i> 0) {
+                character2 = text.charAt(i-1);
+            }
+            if (Character.isDigit(character) && character2 == '-') {
+                return true; 
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         klient client = new klient();
         client.startConnection("127.0.0.1", 6666);
@@ -32,18 +82,38 @@ public class klient {
          try (Scanner scanner = new Scanner(System.in)) {
             String inputLine;
             while (true) {
-                System.out.print("Enter message for server (or 'quit' to end): ");
+                System.out.print("Enter message for server (or 'exit' to end): ");
                 inputLine = scanner.nextLine();
-                if ("quit".equalsIgnoreCase(inputLine)) {
-                    break;
-                }
-                String response = client.sendMessage(inputLine);
-                if(inputLine.equals("bye")){ 
+                if(inputLine.toLowerCase().startsWith("exit")){
                     client.stopConnection();
                     break;
-                    }
+                }
+                if (!inputLine.contains(":")){
+                    throw new errorMessage("Given wrong format of message. Example: 'Message :seconds '");
+                }
+                if (inputLine.equals("") ) { 
+                    System.out.println("Given empty message. Message not sent to the server");
+                    continue;
+                } else if (inputLine.length() > 200 ) {
+                    System.out.println("Given message is above 200 characters. Message not sent to the server");    
+                    continue;
+                } 
+                int time = convertStringToTime(inputLine);
+                if (time == -1 || time == 0) {
+                    System.out.println("Given time is wrong. Message not sent to the server");
+                    continue;
+                } else if(time == -2) {
+                    System.out.println("Given time is not intiger. Message not sent to the server");
+                    continue;
+                }
+
+                else {
+                String response = client.sendMessage(inputLine);
                 System.out.println(response);
+                }
             }
+        } catch (IOException | errorMessage e) {
+            System.out.println("Error: " + e.getMessage());
         }
 }
 }
