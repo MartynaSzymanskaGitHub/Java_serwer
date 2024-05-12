db28628feef3d88d5b939826e8bb0024ccecebb3
Martyna Szymańska <martynaszymanska02@gmail.com>
2024-05-12 20:55:11 +0200

    fix serwer issue

--- serwer.java
+++ serwer.java
@@ -4,16 +4,16 @@ import java.net.*;
 public class serwer {
 private ServerSocket serverSocket;
 public int port = 6666;
-
- void start() throws IOException, ClassNotFoundException{
+public int counterClients = 0;
+ void start() throws IOException{
     serverSocket = new ServerSocket(port);
-    int counterClients = 0;
+   
     System.out.println("Welcome to my server");
         while (true){
-             Socket clientSocket = serverSocket.accept();
-            new Thread(new klientHandler(clientSocket)).start();
-            counterClients++;
-            System.out.println("Waiting for the client request. Current connected ("+counterClients+")\n");
+             Socket clientSocket = serverSocket.accept(); 
+            new Thread(new klientHandler(clientSocket,this)).start();
+            System.out.println("Client connected");
+           
         }
 }
 
@@ -21,12 +21,10 @@ public void stop() throws IOException {
             serverSocket.close();
         }
 
- public static void main(String[] args) throws IOException, ClassNotFoundException {
+ public static void main(String[] args) throws IOException {
         serwer server = new serwer();
         server.start();
     }
-
-
 }
 
     
